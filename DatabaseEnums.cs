0846a7ed805ae1bbd3e2a49ae90efdde0859301b
Mike875 <51370969+Mike875@users.noreply.github.com>
2020-11-27 15:36:33 +0000

    Create DatabaseEnums.cs

---
+++ DatabaseEnums.cs
@@ -0,0 +1,31 @@
+namespace DatabaseInterface.Enums
+{
+    /// <summary>
+    /// A numeric reference to each of the connection strings available in the DatabaseManager class
+    /// </summary>
+    public enum EConnectionString
+    {
+        Live = 0,
+        Craft = 1,
+        API = 2,
+        DevOps = 3,
+        DevChris = 4,
+        DevDanR = 5,
+        DevDanJ = 6,
+        DevDuncan = 7,
+        DevEmily = 8,
+        DevJack = 9,
+        DevJoe = 10,
+        DevLee = 11,
+        DevMichael = 12,
+        DevNathan = 13,
+        DevPaul = 14,
+        DevRichard = 15,
+        MonthEnd = 16,
+        NET2 = 17,
+        PWRESET = 18,
+        WiggumSQLExpress = 19,
+        XMLSoapTest = 20,
+        HibbertSQLExpress = 21
+    }
+}
