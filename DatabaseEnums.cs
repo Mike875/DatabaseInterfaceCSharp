4f5ff124581dbd37d1f502860453a67667ad1c65
Mike875 <51370969+Mike875@users.noreply.github.com>
2020-11-27 15:47:44 +0000

    Update DatabaseEnums.cs

--- DatabaseEnums.cs
+++ DatabaseEnums.cs
@@ -6,26 +6,6 @@ namespace DatabaseInterface.Enums
     public enum EConnectionString
     {
         Live = 0,
-        Craft = 1,
-        API = 2,
-        DevOps = 3,
-        DevChris = 4,
-        DevDanR = 5,
-        DevDanJ = 6,
-        DevDuncan = 7,
-        DevEmily = 8,
-        DevJack = 9,
-        DevJoe = 10,
-        DevLee = 11,
-        DevMichael = 12,
-        DevNathan = 13,
-        DevPaul = 14,
-        DevRichard = 15,
-        MonthEnd = 16,
-        NET2 = 17,
-        PWRESET = 18,
-        WiggumSQLExpress = 19,
-        XMLSoapTest = 20,
-        HibbertSQLExpress = 21
+        Dev = 1
     }
 }
