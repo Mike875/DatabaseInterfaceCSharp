bd9e7a3c2c03d77d11dc95a1412b8277c936d2b1
Mike875 <51370969+Mike875@users.noreply.github.com>
2020-11-27 15:36:17 +0000

    Create RecordSetEnums.cs

---
+++ RecordSetEnums.cs
@@ -0,0 +1,12 @@
+namespace DatabaseInterface.Enums
+{
+    /// <summary>
+    /// Used to represent the state of a recordset
+    /// </summary>
+    public enum EState
+    {
+        Closed,
+        Executing,
+        Open
+    }
+}
